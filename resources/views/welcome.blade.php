95e071d82ea5b44879416d10a91c6b9215258a74
Federica Mudu <federica.mudu.1993@gmail.com>
2024-11-25 12:04:16 +0100

    aggiunto progetto nella repo personale

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid min-vh-100 bgBackgroundWelcome py-5">
         <div class="row justify-content-center">
             <div class="col-12 col-md-6">
-                <span class="titleWelcome">Museum ART</span>
+                <span class="titleWelcome">Museum Art</span>
             </div>
         </div>
     </div>
